dc48c6d2c8807e46d3994bd629c46582c8d39ffc
Justin Castro <juscas13@gmail.com>
2024-05-20 18:05:36 -0500

    added icons and banner image

--- reddit-clone/src/App.jsx
+++ reddit-clone/src/App.jsx
@@ -1,5 +1,38 @@
 import data from "./data.json";
 import Post from "./Post";
+import {
+  MagnifyingGlass,
+  Bell,
+  Plus,
+  ChatCircleDots,
+  EggCrack,
+  House,
+  ArrowCircleUpRight,
+  RedditLogo,
+  Megaphone,
+  Question,
+  BookOpen,
+  Wrench,
+  MicrophoneStage,
+  UsersThree,
+  HourglassLow,
+  CirclesThreePlus,
+  Scroll,
+  Scales,
+  Article,
+  CaretUp,
+  CaretDown,
+  DotsThree,
+  X,
+  PushPinSimple,
+  ArrowFatUp,
+  ArrowFatDown,
+  Export,
+  Medal,
+  ArrowSquareOut,
+} from "phosphor-react";
+import Avvvatars from "avvvatars-react";
+import "./App.css";
 import { useState, useEffect } from "react";
 
 function App() {
@@ -14,20 +47,36 @@ function App() {
   return (
     <>
       <div className="header">
-        <span>knowit</span>
+        <div className="logo">
+          <RedditLogo size={32} />
+          <span>knowit</span>
+        </div>
         <div className="searchbar">
-          <button>Search</button>
-          <input type="text" placeholder="Search in r/Helldivers" />
+          <MagnifyingGlass size={20} />
+          <input
+            className="search-input"
+            type="text"
+            placeholder="Search in r/Helldivers"
+          />
         </div>
         <div className="actions">
-          <button>Messages</button>
-          <button>Create Post</button>
-          <button>Notifications</button>
-          <button>Login</button>
+          <EggCrack size={24} />
+          <ChatCircleDots size={24} />
+          <Plus size={24} />
+          <Bell size={24} />
+          <Avvvatars style="shape" />
         </div>
       </div>
-      <div>
-        <img src="#" alt="" />
+        <img
+          src="https://styles.redditmedia.com/t5_2ya0t/styles/bannerBackgroundImage_vpo0ffwsuryc1.png"
+          alt=""
+        />
+      <div className="thread-title">
+        <img className="thread-icon"
+          src="https://styles.redditmedia.com/t5_2ya0t/styles/communityIcon_ql5iyxjfuvic1.jpg?format=pjpg&s=c2ffada26a84a71ed5b3ec07bba0a1f825ce9833"
+          alt=""
+          width={80}
+        />
         <h1>r/Helldivers</h1>
       </div>
       <Post />
