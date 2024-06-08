d376bb4ed3688e548eeda76259af98b7d5cb8a58
Justin Castro <juscas13@gmail.com>
2024-06-08 17:03:53 -0500

    change classNames and focus more on mobile

--- reddit-clone/src/App.jsx
+++ reddit-clone/src/App.jsx
@@ -30,10 +30,12 @@ import {
   Export,
   Medal,
   ArrowSquareOut,
+  List,
 } from "phosphor-react";
 import Avvvatars from "avvvatars-react";
 import "./App.css";
 import { useState, useEffect } from "react";
+import Banner from "./assets/bannerBackground.png";
 
 function App() {
   // const TimestampConverter = ({ timestamp }) => {
@@ -46,46 +48,71 @@ function App() {
 
   return (
     <>
-      <div className="header">
-        <div className="logo">
-          <RedditLogo size={32} />
-          <span>knowit</span>
+      {/* Header */}
+      <div className="header grid grid-cols-header">
+        <div className="header-nav__mobile">
+          <List size={24} />
         </div>
-        <div className="searchbar">
-          <MagnifyingGlass size={20} />
+        <div className="header__logo">
+          <div classsName="header__logo mobile">
+            <a className="flex header-logo__button" href="#">
+              <RedditLogo size={32} />
+            </a>
+          </div>
+          <div className="header__logo desktop">
+            <a className="flex header-logo__button" href="#">
+              <RedditLogo size={32} />
+              <span>knowit</span>
+            </a>
+          </div>
+        </div>
+        <div className="header__searchbar">
+          <MagnifyingGlass size={24} />
           <input
-            className="search-input"
-            type="text"
+            className="header__search-input mobile"
+            placeholder="Search Knowit"
+          />
+          <input
+            className="header__search-input desktop"
             placeholder="Search in r/Helldivers"
           />
         </div>
-        <div className="actions">
-          <EggCrack size={24} />
-          <ChatCircleDots size={24} />
-          <Plus size={24} />
-          <Bell size={24} />
-          <Avvvatars style="shape" />
+        <div className="header__actions flex align-center">
+          <EggCrack className="desktop" size={24} />
+          <a className="flex header-action__button" href="#">
+            <ChatCircleDots size={24} />
+          </a>
+          <a className="flex header-action__button" href="#">
+            <Plus size={24} />
+          </a>
+          <a className="flex header-action__button" href="#">
+            <Bell size={24} />
+          </a>
+          <a className="flex header-action__button" href="#">
+            <Avvvatars style="shape" />
+          </a>
         </div>
       </div>
-      <div className="grid navigation">
-        <div className="container">
+      {/* Main Container */}
+      <div className="container">
+        <div className="navigation-container grid navigation-grid__side-navigation">
           <ul>
-            <li> Home</li>
+            <li>Home</li>
             <li>Popular</li>
             <li>All</li>
           </ul>
           <ul>
-            <li> Create a custom feed</li>
+            <li>Create a custom feed</li>
           </ul>
           <ul>
-            <li> Home</li>
+            <li>Home</li>
             <li>Popular</li>
             <li>All</li>
             <li>All</li>
             <li>All</li>
           </ul>
           <ul>
-            <li> About Reddit</li>
+            <li>About Reddit</li>
             <li>Advertise</li>
             <li>Help</li>
             <li>Blog</li>
@@ -93,43 +120,52 @@ function App() {
             <li>Press</li>
           </ul>
         </div>
-        <div className="container">
-          <img
-            className="banner"
-            src="https://styles.redditmedia.com/t5_2ya0t/styles/bannerBackgroundImage_vpo0ffwsuryc1.png"
-            alt=""
-          />
-          <div className="thread-title flex">
-            <div className="flex">
+        <div className="main-container">
+          <img className="main-container__banner" src={Banner} alt="" />
+          <div className="main-container__intro">
+            <div className="main-container__thread-content">
               <img
-                className="thread-icon"
+                className="main-container__icon"
                 src="https://styles.redditmedia.com/t5_2ya0t/styles/communityIcon_ql5iyxjfuvic1.jpg?format=pjpg&s=c2ffada26a84a71ed5b3ec07bba0a1f825ce9833"
                 alt=""
                 width={80}
               />
+              <div className="main-container__thread-info">
                 <h1>r/Helldivers</h1>
-            </div>
-              <div className="flex">
-              <button>Create a post</button>
-              <button>
-                <Bell />
-              </button>
-              <button>Joined</button>
-              <button>...</button>
               </div>
-          </div>
-          <div className="grid body">
-            <Post />
-            <div className="container">
-              <h3>Helldivers</h3>
-              <p>
-                A subreddit dedicated to HELLDIVERS and HELLDIVERS 2, intense
-                co-op shooters set in a satirical dystopian future where you
-                play as one of mankind's elite soldiers determined to spread
-                managed democracy. Developed by Arrowhead Game Studios.
-              </p>
+              <div className="thread-title__actions flex">
+                <button className="thread-action action-button__primary flex align-center">
+                  <span className="flex align-center">
+                    <Plus size={18} />
+                    Create a post
+                  </span>
+                </button>
+                <button className="thread-action action-button__primary flex">
+                  <span className="flex align-center">
+                    <Bell size={18} />
+                  </span>
+                </button>
+                <button className="thread-action action-button__primary flex align-center">
+                  <span className="flex align-center">Joined</span>
+                </button>
+                <button className="thread-action action-button__primary flex align-center">
+                  <span className="flex align-center">
+                    <DotsThree size={24} />
+                  </span>
+                </button>
+              </div>
             </div>
           </div>
+          <div className="thread-body__about-feed">
+            <h3>Helldivers</h3>
+            <p>
+              A subreddit dedicated to HELLDIVERS and HELLDIVERS 2, intense
+              co-op shooters set in a satirical dystopian future where you play
+              as one of mankind's elite soldiers determined to spread managed
+              democracy. Developed by Arrowhead Game Studios.
+            </p>
+          </div>
+          <div className="thread-body">{/* <Post /> */}</div>
         </div>
       </div>
     </>
